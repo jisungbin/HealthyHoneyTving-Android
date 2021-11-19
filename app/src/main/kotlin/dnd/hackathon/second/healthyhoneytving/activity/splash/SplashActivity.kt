ad2867d02c404c4a9d63d58feb0d8b27496d73e1
jisungbin <ji@sungb.in>
2021-11-19 23:51:22 +0900

    Update: splash activity

--- app/src/main/kotlin/dnd/hackathon/second/healthyhoneytving/activity/splash/SplashActivity.kt
+++ app/src/main/kotlin/dnd/hackathon/second/healthyhoneytving/activity/splash/SplashActivity.kt
@@ -2,6 +2,15 @@
  * HealthyHoneyTving © DND 해커톤 2조. all rights reserved.
  * HealthyHoneyTving license is under the MIT.
  *
+ * [SplashActivity.kt] created by Ji Sungbin on 21. 11. 19. 오후 11:26
+ *
+ * Please see: https://github.com/DND-hackathon/HealthyHoneyTving-Android/blob/dev/LICENSE.
+ */
+
+/*
+ * HealthyHoneyTving © DND 해커톤 2조. all rights reserved.
+ * HealthyHoneyTving license is under the MIT.
+ *
  * [SplashActivity.kt] created by Ji Sungbin on 21. 11. 19. 오후 10:50
  *
  * Please see: https://github.com/DND-hackathon/HealthyHoneyTving-Android/blob/dev/LICENSE.
@@ -13,8 +22,27 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import dnd.hackathon.second.healthyhoneytving.R
 import dnd.hackathon.second.healthyhoneytving.theme.MaterialTheme
+import dnd.hackathon.second.healthyhoneytving.theme.colors
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : ComponentActivity() {
@@ -30,5 +58,31 @@ class SplashActivity : ComponentActivity() {
 
     @Composable
     private fun Content() {
+        var rotate by remember { mutableStateOf(90F) }
+        var offsetX by remember { mutableStateOf(0.dp) }
+        var showLabel by remember { mutableStateOf(false) }
+
+        LaunchedEffect(Unit) {
+            rotate = 0F
+            offsetX = (-20).dp
+            showLabel = true
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colors.primary),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(50.dp)
+                    .rotate(rotate)
+                    .offset(x = offsetX),
+                painter = painterResource(R.drawable.ic_round_logo_24),
+                contentDescription = null
+            )
+        }
     }
 }
