6d150d056ad1c0886be01855b8ab86f65a754f27
jisungbin <ji@sungb.in>
2021-11-19 21:50:39 +0900

    Chore: gradle init

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:${Versions.Essential.Gradle}")
+        classpath("com.android.tools.build:gradle:7.1.0-beta03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.Essential.Kotlin}")
     }
 }
