694705b26a9dc120de44de931a29592593395d69
jisungbin <ji@sungb.in>
2021-11-20 04:44:39 +0900

    Create: main activity

---
+++ app/src/main/kotlin/dnd/hackathon/second/healthyhoneytving/activity/main/MainActivity.kt
@@ -0,0 +1,14 @@
+/*
+ * HealthyHoneyTving © DND 해커톤 2조. all rights reserved.
+ * HealthyHoneyTving license is under the MIT.
+ *
+ * [MainActivity.kt] created by Ji Sungbin on 21. 11. 20. 오전 4:44
+ *
+ * Please see: https://github.com/DND-hackathon/HealthyHoneyTving-Android/blob/dev/LICENSE.
+ */
+
+package dnd.hackathon.second.healthyhoneytving.activity.main
+
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity()
