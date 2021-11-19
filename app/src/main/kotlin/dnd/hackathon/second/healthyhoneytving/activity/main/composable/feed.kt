e1379363b28d11cb4c34f94357e770cd3873cf35
jisungbin <ji@sungb.in>
2021-11-20 08:49:14 +0900

    Update: main activity composable

--- app/src/main/kotlin/dnd/hackathon/second/healthyhoneytving/activity/main/composable/feed.kt
+++ app/src/main/kotlin/dnd/hackathon/second/healthyhoneytving/activity/main/composable/feed.kt
@@ -9,7 +9,19 @@
 
 package dnd.hackathon.second.healthyhoneytving.activity.main.composable
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import dnd.hackathon.second.healthyhoneytving.activity.main.model.Feed
 
 @Composable
@@ -18,5 +30,21 @@ fun LazyFeed() {
 
 @Composable
 private fun Feed(feed: Feed) {
-
+    Column(
+        modifier = Modifier
+            .background(color = Color.White)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(vertical = 8.dp, horizontal = 16.dp)
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = feed.ownerUid)
+        }
+    }
 }
