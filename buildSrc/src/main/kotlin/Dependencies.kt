dbe25eb573fc2b7ac065a7601032efdc6c78f5d2
jisungbin <ji@sungb.in>
2021-11-20 17:03:33 +0900

    제발 되라

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object Application {
     const val minSdk = 24
-    const val targetSdk = 31
-    const val compileSdk = 31
+    const val targetSdk = 30
+    const val compileSdk = 30
     const val jvmTarget = "11"
     const val versionCode = 1
     const val versionName = "1.0.0"
