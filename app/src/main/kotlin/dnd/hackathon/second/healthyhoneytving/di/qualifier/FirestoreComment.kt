f70126c489b64a11c3ab9df52b2978ab46f6f0bb
jisungbin <ji@sungb.in>
2021-11-20 15:08:53 +0900

    서버 작업 시작

---
+++ app/src/main/kotlin/dnd/hackathon/second/healthyhoneytving/di/qualifier/FirestoreComment.kt
@@ -0,0 +1,16 @@
+/*
+ * HealthyHoneyTving © DND 해커톤 2조. all rights reserved.
+ * HealthyHoneyTving license is under the MIT.
+ *
+ * [FirestoreComment.kt] created by Ji Sungbin on 21. 11. 20. 오후 3:08
+ *
+ * Please see: https://github.com/DND-hackathon/HealthyHoneyTving-Android/blob/dev/LICENSE.
+ */
+
+package dnd.hackathon.second.healthyhoneytving.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class FirestoreComment
